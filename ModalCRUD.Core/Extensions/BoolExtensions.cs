cdf38e9dfa6aa177ec9f46fba416b63afe723f43
John Dave Dalmao <johndavedalmao@gmail.com>
2022-03-10 13:29:32 +0800

    created a new class to store bool extension methods, which currently has a method to verify a hashed password from the database

---
+++ ModalCRUD.Core/Extensions/BoolExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModalCRUD.Core.Extensions
+{
+    public static class BoolExtensions
+    {
+        public static bool VerifyHash(this string currentUserPassword, string hashedPasswordFromDb)
+        {
+            return BCrypt.Net.BCrypt.Verify(currentUserPassword, hashedPasswordFromDb);
+        }
+    }
+}
