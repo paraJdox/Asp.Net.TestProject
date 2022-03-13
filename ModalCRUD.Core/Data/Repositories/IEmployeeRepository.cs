6d94783dd0f82fe5bf04304abb407f31238b4270
John Dave Dalmao <johndavedalmao@gmail.com>
2022-03-13 14:50:07 +0800

    moved IEmployeeRepository and IUserRepository repository interfaces in ModalCRUD.Core project

---
+++ ModalCRUD.Core/Data/Repositories/IEmployeeRepository.cs
@@ -0,0 +1,18 @@
+﻿using ModalCRUD.Core.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModalCRUD.Core.Data.Repositories
+{
+    public interface IEmployeeRepository
+    {
+        Task<Employee> CreateAsync(Employee employee);
+        Task DeleteAsync(int id);
+        Task<IEnumerable<Employee>> GetAllAsync();
+        Task<Employee?> GetByIdAsync(int id);
+        Task<Employee> UpdateAsync(Employee employee);
+    }
+}
---
+++ ModalCRUD.Core/Data/Repositories/IUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using ModalCRUD.Core.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModalCRUD.Core.Data.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<User> CreateAsync(User user);
+        Task<IEnumerable<User>> GetAllAsync();
+        Task<bool> UsernameExists(string username);
+        Task<User> ValidateUserAsync(User inputUser);
+    }
+}
