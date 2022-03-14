9632fcc69e4807cd45eeba03d7472ff1c7766787
John Dave Dalmao <johndavedalmao@gmail.com>
2022-03-14 16:13:20 +0800

    created a new service: IUserService

---
+++ ModalCRUD.Core/Services/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using ModalCRUD.Core.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModalCRUD.Core.Services
+{
+    public interface IUserService
+    {
+        User SignUp(User user);
+        User SignIn(User user);
+    }
+}
